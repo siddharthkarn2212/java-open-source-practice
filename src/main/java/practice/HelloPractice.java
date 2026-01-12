e110c9d42827468b4635a578d144f940f35ce8fc
siddharth karn <siddharthkarn1204@gmail.com>
2026-01-12 12:08:27 +0530

    Add initial Java practice file

---
+++ src/main/java/practice/HelloPractice.java
@@ -0,0 +1,7 @@
+package practice;
+
+public class HelloPractice {
+    public static void main(String[] args) {
+        System.out.println("Practicing Java and open source for GSoC 2026");
+    }
+}
